12dd02bdb65b20963f9a02d2656ecb6e6d0e2a53
Thanzeela K A <thanzeela.ka@experionglobal.local>
2022-11-06 22:17:05 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,108 @@
+
+// variable datatypes , operators conditional statements, loops class
+// var let const
+
+
+
+
+
+
+
+
+
+
+
+
+greet("meera")
+//010010101001010
+const age = 18;
+
+// memory | code
+// function greet(name){
+//     console.log(name)
+// }
+// let charage = undefined
+
+
+
+
+
+
+
+
+
+
+
+// 10 lines
+let charage = 10;
+// datatypes
+
+// string number boolean array object undefined null
+
+let name = "mario";
+
+let char_age = 7;
+
+let ismarried = false;
+
+let array = [1,2,3,4,"samad",true,[1,2,3,4,[1,2,3,4,[1,2,3,[1,2,29]]]],{address:"malappuram"},undefined,"",function () {console.log('nithu')}]
+console.log(function () {console.log('nithu')}())
+console.log(array[7].address)
+let Person = {
+    name:'so'
+}
+
+let un;
+// intrepreted
+let prince = {
+    place:"kottayam",
+    log:function(){
+        console.log('function called')
+    }
+}
+console.log(prince.place)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let person = {
+    name:"vismaya mg",
+    age : 26,
+    ismarried:true,
+    fav_food:['biryani','chicken noodles',[1,2,3],200],
+    log:function (){
+        console.log(this.name+" is eating "+this.fav_food[1])
+    },
+    marks:{
+        physics:10,
+        maths:10
+    }
+}
+
+
+function greet(){
+    console.log('hai')
+}
+
+let arrow = ()=>{
+    console.log('arrow func')
+}
+arrow();
